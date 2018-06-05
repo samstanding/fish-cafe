a2170209353195e54b84782a5a1fdf93536dbe60
Sam Standing <samzstanding@gmail.com>
2018-06-05 13:10:51 -0500

    moved home data to javascript, fixed bootstrap bug

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  //cafe name, address and hours saved in js
+  //simulates an external db or api that data would be saved in
+  name ='Frankie\'s Fish Cafe';
+  address = '8257 Ridgeview St. Darien, CT 06820';
+
+  hours = {
+    m: '12:00pm - 11:00pm',
+    t: '12:00pm - 11:00pm',
+    w: '12:00pm - 11:00pm',
+    th: '12:00pm - 11:00pm',
+    f: '12:00pm - 12:00am',
+    s: '12:00pm - 12:00am',
+    su: '12:00pm - 11:00pm',
+  }
 
   constructor() { }
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { MENUITEMS } from '../menuitems';
 
+
+let rawBar = [];
+let soups = [];
+let salads = [];
+let starters = [];
+let sandies = [];
+let works = [];
+let sides = [];
+
+
+for (let item of MENUITEMS) {
+  if(item.type === 'Raw Bar') {
+    rawBar.push(item);
+  } else if (item.type === 'soup') {
+    soups.push(item); 
+  } else if (item.type === 'salad') {
+    salads.push(item);
+  } else if (item.type === 'starter') {
+    starters.push(item);
+  } else if(item.type === 'fries on the side') {
+    sandies.push(item);
+  } else if (item.type === 'the works') {
+    works.push(item);
+  } else if (item.type === 'sides') {
+    sides.push(item);
+  }
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
+
 export class MenuComponent implements OnInit {
-  menuItems = MENUITEMS;
+ rawBar = rawBar;
+ soups = soups;
+ salads = salads;
+ starters = starters;
+ sandies = sandies;
+ works = works;
+ sides = sides;
+  
+ 
 
   constructor() { }
 
