ca755fc31b4a820d93d5474e6e0356399ebe6c4d
Sam Standing <samzstanding@gmail.com>
2018-06-06 12:08:15 -0500

    added comments and altered navbar

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  //values to be displayed on the contact page
   phone = '646-641-2212';
   message = 'We only take reservations for parties of 4 or more'
 
